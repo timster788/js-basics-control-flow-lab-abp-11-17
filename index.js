e9315734b1b2bd947fc743ca5daefb3c849b83f9
timster788 <timster78811@gmail.com>
2018-03-21 23:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function scuberGreetingForFeet(distance){
  } else if (distance > 2000 && distance < 2500){
    return 'I will gladly take your thirty bucks.'
  } else if (distance > 2500){
-   return 'No can do'
+   return 'No can do.'
  }
  }
